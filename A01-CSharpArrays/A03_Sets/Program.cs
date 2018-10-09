b694c4575088983dc8623d39b754fa6936c63890
Fernando Jesus <fernando.js8@outlook.com>
2018-10-08 23:49:17 -0300

    Aula 03 em andamento..

---
+++ A01-CSharpArrays/A03_Sets/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A03_Sets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISet<string> alunos = new HashSet<string>();
+
+            alunos.Add("Vanessa Tonini");
+            alunos.Add("Ana Losnak");
+            alunos.Add("Rafael Nercessian");
+
+            Console.WriteLine(alunos);
+            Console.WriteLine(string.Join(", ", alunos));
+
+            alunos.Add("Priscila Stuani");
+            alunos.Add("Rafael Rollo");
+            alunos.Add("Fabio Gushiken");
+            alunos.Remove("Ana Losnak");
+            alunos.Add("Marcelo Oliveira");
+
+            Console.WriteLine(string.Join(", ", alunos));
+
+            List<string> alunosEmLista = new List<string>(alunos);
+
+            alunosEmLista.Sort();
+
+            Console.WriteLine(string.Join(", ", alunosEmLista));
+        }
+    }
+}
