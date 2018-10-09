5b443db35f65eccc0ce5924223f66de54632e6bd
Fernando Jesus <fernando.js8@outlook.com>
2018-10-08 22:37:01 -0300

    Aula 02 Finalizada OK!

---
+++ A01-CSharpArrays/A01-CSharpArrays/Aula.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A01_CSharpArrays
+{
+    public class Aula : IComparable
+    {
+        private string titulo;
+        private int tempo;
+
+        public Aula(string titulo, int tempo)
+        {
+            this.titulo = titulo;
+            this.tempo = tempo;
+        }
+
+        public string Titulo { get => titulo; set => titulo = value; }
+        public int Tempo { get => tempo; set => tempo = value; }
+
+        public int CompareTo(object obj)
+        {
+            Aula that = obj as Aula;
+            return this.Titulo.CompareTo(that.Titulo);
+        }
+
+        public override string ToString()
+        {
+            return $"Título: {Titulo}, Tempo: {Tempo}";
+        }
+
+        public void SortTempo(List<Aula> aulas)
+        {
+            aulas.Sort((x, y) => x.Tempo.CompareTo(y.tempo));
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aulas);
+            }
+        }
+    }
+}
--- A01-CSharpArrays/A01-CSharpArrays/Program.cs
+++ A01-CSharpArrays/A01-CSharpArrays/Program.cs
@@ -10,10 +10,122 @@ namespace A01_CSharpArrays
     {
         static void Main(string[] args)
         {
-            String aula1 = "Aula Intro";
-            String aula2 = "Modelando classes";
-            String aula3 = "Trabalhando com Conjuntos";
+            var aula1 = new Aula("Introdução a coleções", 20);
+            var aula2 = new Aula("Modelando a Classe Aula", 18);
+            var aula3 = new Aula("Trabalhando com Conjuntos", 16);
 
+            Curso cSharpColecoes = new Curso("C# Collections", "Marcel Oliveira");
+            cSharpColecoes.Adiciona(new Aula("Trabalhando com Listas", 20));
+            ImprimirAula(cSharpColecoes.Aulas);
+
+            // adicionar 2 aulas
+            cSharpColecoes.Adiciona(new Aula("Criando uma aula", 20));
+            cSharpColecoes.Adiciona(new Aula("Modelando com coleções", 19));
+
+            // imprimir as aulas
+            ImprimirAula(cSharpColecoes.Aulas);
+
+            // ordenar lista de aulas
+
+            //copiar esta lista para uma nova lista
+            List<Aula> aulasCopiadas = new List<Aula>(cSharpColecoes.Aulas);
+
+            // ordenar a copia
+            aulasCopiadas.Sort();
+
+            ImprimirAula(aulasCopiadas);
+
+            // totalizar o tempo do curso
+            Console.WriteLine(cSharpColecoes.TempoTotal);
+
+            Console.WriteLine(cSharpColecoes);
+
+        }
+
+        #region Aulas
+
+        private static void AulaAnterior(Aula aula1, Aula aula2, Aula aula3)
+        {
+            List<Aula> aulas = new List<Aula>();
+
+            aulas.Add(aula1);
+            aulas.Add(aula2);
+            aulas.Add(aula3);
+
+            ImprimirAula(aulas);
+
+            //aulas.Sort();
+
+            ImprimirAula(aulas);
+
+            aulas.Sort((x, y) => x.Tempo.CompareTo(y.Tempo));
+
+            ImprimirAula(aulas);
+
+            TesteAulas teste1 = new TesteAulas("Pessoa", 123456);
+            TesteAulas teste2 = new TesteAulas("Siclano", 654321);
+            TesteAulas teste3 = new TesteAulas("Beltrano", 987654321);
+
+            List<TesteAulas> listaTeste = new List<TesteAulas>();
+
+            listaTeste.Add(teste1);
+            listaTeste.Add(teste2);
+            listaTeste.Add(teste3);
+
+            TesteImprimir(listaTeste);
+
+            listaTeste.Sort();
+            TesteImprimir(listaTeste);
+        }
+
+        private static void TesteImprimir(List<TesteAulas> listaTeste)
+        {
+            Console.WriteLine();
+
+            foreach (var teste in listaTeste)
+            {
+                Console.WriteLine(teste);
+            }
+        }
+
+        class TesteAulas : IComparable
+        {
+            private string nome;
+            private int cpf;
+
+            public TesteAulas(string nome, int cpf)
+            {
+                this.nome = nome;
+                this.cpf = cpf;
+            }
+
+            public string Nome { get => nome; set => nome = value; }
+            public int Cpf { get => cpf; set => cpf = value; }
+
+            public int CompareTo(object obj)
+            {
+                TesteAulas castObj = obj as TesteAulas;
+                return Nome.CompareTo(castObj.Nome);
+            }
+
+            public override string ToString()
+            {
+                return $"Nome: {Nome}, CPF: {Cpf}";
+            }
+
+        }
+
+        private static void ImprimirAula(IList<Aula> aulas)
+        {
+            Console.Clear();
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
+        }
+
+        private static void Aula02(string aula1, string aula2, string aula3)
+        {
             List<string> aulas = new List<string>();
 
             aulas.Add(aula1);
@@ -27,13 +139,49 @@ namespace A01_CSharpArrays
 
             ImprimirLista(aulas);
 
-            Console.WriteLine("A Primeira aula é '" + aulas[0] +"'");
+            Console.WriteLine("A Primeira aula é '" + aulas[0] + "'");
             Console.WriteLine($"A Última aula é '{aulas[aulas.Count - 1]}'");
 
             Console.WriteLine();
 
             Console.WriteLine("A Primeira aula é " + aulas.First());
             Console.WriteLine("A Última aula é " + aulas.Last());
+
+            aulas[0] = "Trabalhando com Listas";
+
+            Console.WriteLine("A primeira aula 'Trabalhando' é:" + aulas.First(aula => aula.Contains("Trabalhando")));
+
+            Console.WriteLine("A última aula 'Trabalhando' é:" + aulas.Last(aula => aula.Contains("Trabalhando")));
+
+            Console.WriteLine("A primeira ou default aula 'contém' é:" + aulas.FirstOrDefault(aula => aula.Contains("Contem")));
+
+            aulas.Reverse();
+            ImprimirLista(aulas);
+            Console.WriteLine();
+
+            aulas.Reverse();
+            ImprimirLista(aulas);
+
+            Console.WriteLine();
+            aulas.RemoveAt(aulas.Count - 1);
+            ImprimirLista(aulas);
+
+            aulas.Add("Conclusão");
+            ImprimirLista(aulas);
+
+            List<string> copia = aulas.GetRange(aulas.Count - 2, 2);
+
+            ImprimirLista(copia);
+
+            List<string> clone = new List<string>(aulas);
+
+            ImprimirLista(clone);
+
+            clone.RemoveRange(clone.Count - 2, 2);
+            ImprimirLista(clone);
+
+            aulas.Sort();
+            ImprimirLista(aulas);
         }
 
         private static void AulaArrays(out string aula1, out string aula2, out string aula3)
@@ -123,5 +271,7 @@ namespace A01_CSharpArrays
         {
             for (int i = 0; i < aulas.Length; i++) Console.WriteLine(aulas[i]);
         }
+
+        #endregion
     }
 }
